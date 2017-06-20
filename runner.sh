895b5344548f19a51a6515deba11d3a8927dd9b1
Jerry Marino <i@jerrymarino.com>
2017-06-19 21:47:49 -0700

    iOS support, Framework Support, and more
    Create a iOS compiler invocation for iOS predicated by the playground
    settings. It searches contents.xcplayground for iOS.
    
    Additionally:
    
    - Leverage the "Sources/" directory which is created by Xcode. The
    playground convention is to put all files here. Setup linker flags and
    rpaths for frameworks in Sources. Include all swift sources in Sources/.
    
    - Derive a build directory based on the playground path.
    
    fixes #1

--- runner.sh
+++ runner.sh
@@ -1,25 +1,85 @@
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $dir
+SRC_ROOT=$(dirname $1)
+PLUGIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Clean all for now. Hack to display errors, and ideally
-# we will not do this!
-rm -rf build
+# Derive a tmp build directory
+BUILD_DIR="/tmp/SwiftPlayground-$( echo $SRC_ROOT | md5 )"
 
-mkdir -p build
+# Remove if it already existed. Errors seem to be swallowed otherwise
+rm -rf $BUILD_DIR
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
 # For now, dump the entire file into main this is likely contents.swift
-cat $1 > build/main.swift
+# Consider writing the buffer instead
+cat $1 > main.swift
 
-xcrun swiftc \
--Xfrontend \
--playground \
--Xfrontend \
--debugger-support \
--module-name Playgound \
--o \
-build/main \
-build/main.swift \
-PlaygroundRuntime.swift
+# Build up a SwiftC invocation and then run
 
-./build/main
+if [[ -d $SRC_ROOT/Sources/ ]]; then
+    # Framework includes, rpath, and source includes
+    PLAYGROUND_FLAGS="-F$SRC_ROOT/Sources/ -I$SRC_ROOT/Sources/ -Xlinker -rpath -Xlinker $SRC_ROOT/Sources/"
+
+    # WARNING! this may be dangerous in some terminals.
+    PLAYGROUND_SOURCES="$( find $SRC_ROOT/Sources/*.swift )"
+else
+    PLAYGROUND_FLAGS=""
+    PLAYGROUND_SOURCES=""
+fi
+
+# Check contents.xcplayground for ios
+grep -q "target-platform='ios'" "$SRC_ROOT/contents.xcplayground"
+if [[ $? == 0 ]]; then
+    # Build and run for iOS
+    xcrun swiftc \
+    -Xfrontend \
+    -debugger-support \
+    -Xfrontend \
+    -playground \
+    -module-name \
+    Playground \
+    -target \
+    x86_64-apple-ios7.0 \
+    -sdk \
+    /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -F \
+    /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/Frameworks \
+    -F \
+    /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/Frameworks \
+    -F \
+    /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/Frameworks \
+    -F \
+    /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/PrivateFrameworks \
+    $PLAYGROUND_FLAGS \
+    -o \
+    main \
+    main.swift \
+    $PLAYGROUND_SOURCES \
+    $PLUGIN_DIR/PlaygroundRuntime.swift
+
+    # Run on the simulator by default we build for x86_64, so use the first
+    # iPhone that isn't a 5.
+    # Find a device that is either (booted) or (shutdown)
+    DEVICE=$( xcrun simctl list | grep 'iPhone [^5] .*(.*).*(.*)' | perl -pe 's/(.*\()(.*)\)+ (.*)/\2/' | sed -n 1p )
+    xcrun simctl spawn $DEVICE $PWD/main
+else
+    # Build and run for the host target
+    # This invocation theoretically shouldn't need any OSX specifics at this
+    # level.
+    xcrun swiftc \
+    -Xfrontend \
+    -debugger-support \
+    -Xfrontend \
+    -playground \
+    -module-name \
+    Playground \
+    $PLAYGROUND_FLAGS \
+    -o \
+    main \
+    main.swift \
+    $PLAYGROUND_SOURCES \
+    $PLUGIN_DIR/PlaygroundRuntime.swift
+
+    ./main
+fi
 
